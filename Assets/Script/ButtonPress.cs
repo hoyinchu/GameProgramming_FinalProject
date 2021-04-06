c615eb405bb8e9563147c60e5b7b90156ea10b3a
izzyconner <conner.i@husky.neu.edu>
2021-04-05 20:33:22 -0400

    Started adding button functionality

---
+++ Assets/Script/ButtonPress.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPress : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] animatedObjects;
+    public string triggerName;
+    bool running = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // display message on UI saying "Press E to press button"
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                foreach (GameObject go in animatedObjects)
+                {
+                    running = !running;
+                    Debug.Log("button pressed");
+                }
+            }
+        }
+    }
+}
